c49e768615d7fe9d65c4548beca4ea9ac2ed7cea
d.fofanov <dmitry.fofanov@roamergames.com>
2023-12-23 22:05:03 +0600

    Comment updated

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -11,7 +11,7 @@ namespace UnitBrains.Player
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             ///////////////////////////////////////
-            // Change this code
+            // Homework 1.3 (1st block, 3rd module)
             ///////////////////////////////////////
             var projectile = CreateProjectile(forTarget);
             AddProjectileToList(projectile, intoList);
