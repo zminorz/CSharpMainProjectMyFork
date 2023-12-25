ff2d9a999e0c8f596b992d01215d650180d5e3db
d.fofanov <dmitry.fofanov@roamergames.com>
2023-12-25 19:45:29 +0600

    B1M3 homework updated

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -7,9 +7,15 @@ namespace UnitBrains.Player
     public class SecondUnitBrain : DefaultPlayerUnitBrain
     {
         public override string TargetUnitName => "Cobra Commando";
+        private const float OverheatTemperature = 2f;
+        private const float OverheatCooldown = 2f;
+        private float _temperature = 0f;
+        private float _cooldownTime = 0f;
         
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
+            var overheatTemperature = OverheatTemperature;
+            
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
             ///////////////////////////////////////
@@ -28,6 +34,34 @@ namespace UnitBrains.Player
                 result.RemoveAt(result.Count - 1);
             return result;
             ///////////////////////////////////////
+        }
+
+        public override void Update(float deltaTime, float time)
+        {
+            if (_temperature > OverheatTemperature)
+            {
+                if (_cooldownTime <= 0f)
+                    _cooldownTime = time + OverheatCooldown;
+
+                if (time > _cooldownTime)
+                {
+                    _temperature = _cooldownTime = 0f;
+                }
+            }
+            else
+            {
+                _temperature = Mathf.Max(0f, _temperature - deltaTime);
+            }
+        }
+
+        private int GetTemperature()
+        {
+            return (int)_temperature;
+        }
+
+        private void IncreaseTemperature()
+        {
+            _temperature += 0.5f;
         }
     }
 }
