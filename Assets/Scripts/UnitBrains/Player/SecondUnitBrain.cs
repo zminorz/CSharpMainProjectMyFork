d8cd1f0dff11833b0248b42e310b8cd2297f453a
d.fofanov <dmitry.fofanov@roamergames.com>
2024-01-09 14:17:27 +0600

    Empty GetNextStep override for the second unit's brain

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -24,6 +24,11 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
         }
 
+        public override Vector2Int GetNextStep()
+        {
+            return base.GetNextStep();
+        }
+
         protected override List<Vector2Int> SelectTargets()
         {
             ///////////////////////////////////////
