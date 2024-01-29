544cd521492779c8d7c724db08d2937db9e09bff
d.fofanov <dmitry.fofanov@roamergames.com>
2024-01-29 15:20:41 +0600

    Pathfinding infrastructure fixed

---
+++ Assets/Scripts/UnitBrains/Pathfinding/BaseUnitPath.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnitBrains.Pathfinding
+{
+    public abstract class BaseUnitPath
+    {
+        public Vector2Int StartPoint => Vector2Int.zero;
+        public Vector2Int EndPoint => Vector2Int.zero;
+    }
+}
---
+++ Assets/Scripts/UnitBrains/Pathfinding/DebugPathOutput.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using View;
+
+namespace UnitBrains.Pathfinding
+{
+    public class DebugPathOutput : MonoBehaviour
+    {
+        [SerializeField] private GameObject cellHighlightPrefab;
+        [SerializeField] private int maxHighlights = 5;
+
+        public BaseUnitPath Path { get; private set; }
+        private readonly List<GameObject> allHighlights = new();
+        private Coroutine highlightCoroutine;
+
+        public void HighlightPath(BaseUnitPath path)
+        {
+            Path = path;
+            while (allHighlights.Count > 0)
+            {
+                DestroyHighlight(0);
+            }
+            
+            if (highlightCoroutine != null)
+            {
+                StopCoroutine(highlightCoroutine);
+            }
+
+            highlightCoroutine = StartCoroutine(HighlightCoroutine(path));
+        }
+
+        private IEnumerator HighlightCoroutine(BaseUnitPath path)
+        {
+            // TODO Implement me
+            yield break;
+        }
+
+        private void CreateHighlight(Vector2Int atCell)
+        {
+            var pos = Gameplay3dView.ToWorldPosition(atCell, 1f);
+            var highlight = Instantiate(cellHighlightPrefab, pos, Quaternion.identity);
+            highlight.transform.SetParent(transform);
+            allHighlights.Add(highlight);
+        }
+
+        private void DestroyHighlight(int index)
+        {
+            Destroy(allHighlights[index]);
+            allHighlights.RemoveAt(index);
+        }
+    }
+}
