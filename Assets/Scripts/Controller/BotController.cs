e1ae1384af8a86441767125fe22ee83e8eeedd75
Nikita Kuznetsov <n.kuznetsov@kefirgames.com>
2024-05-18 17:24:04 +0300

    Fedor's coment

--- Assets/Scripts/Controller/BotController.cs
+++ Assets/Scripts/Controller/BotController.cs
@@ -6,9 +6,7 @@ using Model;
 using Model.Config;
 using UnityEngine;
 using Utilities;
-//hello
-//How are you?
-//i'm fine
+//hello, I'm Fedor
 namespace Controller
 {
     public class BotController
