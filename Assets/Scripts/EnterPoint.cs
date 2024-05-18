70f6c6a733936e5746dfb82355ecf58527dc540f
Nikita Kuznetsov <n.kuznetsov@kefirgames.com>
2024-05-18 18:11:57 +0300

    chg: timescale value

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -10,7 +10,7 @@ public class EnterPoint : MonoBehaviour
 {
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
-    private float _timeScale = 1;
+    private float _timeScale = 5;
     
     //Привет, здесь был я!
     /*/
