018fb3f8ba6302307f0128005e60bc2b78a5b836
Nikita Kuznetsov <n.kuznetsov@kefirgames.com>
2024-05-18 19:33:54 +0300

    chg: projectile traectory

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -29,12 +29,13 @@ namespace Model.Runtime.Projectiles
             ///////////////////////////////////////
             // Insert you code here
             ///////////////////////////////////////
+            float maxHeight = totalDistance * 0.6f;
 
-
+            localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
             ///////////////////////////////////////
             // End of the code to insert
             ///////////////////////////////////////
-            
+
             Height = localHeight;
             if (time > StartTime + _timeToTarget)
                 Hit(_target);
